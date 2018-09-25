8590ea93af562f75eea45d5a6f8a8eb6c0f12fa7
JustinClough <justin.clough1@gmail.com>
2018-09-25 09:20:55 -0700

    Fixes typos in do_config and CMakeLists

--- increase_smb_order/do_config.sh
+++ increase_smb_order/do_config.sh
@@ -2,7 +2,7 @@ export BUILD_DIR=/home/jlclough/research/high_speed_panel/increase_smb_order/bui
 export SRC_DIR=/home/jlclough/research/high_speed_panel/increase_smb_order/src
 export SCOREC_INC=/usr/local/include
 
-cd $BUILD_DIRA
+cd $BUILD_DIR
 cmake \
   -D SRC_DIR=${SRC_DIR} \
   -D SCOREC_INC=${SCOREC_INC} \
