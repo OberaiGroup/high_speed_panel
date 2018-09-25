d99d735d4bbde10b0df78900160aca69ba8029cc
JustinClough <justin.clough1@gmail.com>
2018-09-25 09:18:23 -0700

    Adds SCOREC_INC to do_config

--- increase_smb_order/do_config.sh
+++ increase_smb_order/do_config.sh
@@ -1,8 +1,10 @@
 export BUILD_DIR=/home/jlclough/research/high_speed_panel/increase_smb_order/build
 export SRC_DIR=/home/jlclough/research/high_speed_panel/increase_smb_order/src
+export SCOREC_INC=/usr/local/include
 
 cd $BUILD_DIRA
 cmake \
   -D SRC_DIR=${SRC_DIR} \
+  -D SCOREC_INC=${SCOREC_INC} \
   -D CMAKE_CXX_FLAGS="-g -O3 -Wall -fPIC -lrt" \
   ../
