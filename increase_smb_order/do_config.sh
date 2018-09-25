e8ddd64373c528eafe0d71d7fa458d75e32d5a5f
JustinClough <justin.clough1@gmail.com>
2018-09-25 09:42:47 -0700

    Fixes build to include scorec lib, headers

--- increase_smb_order/do_config.sh
+++ increase_smb_order/do_config.sh
@@ -1,10 +1,13 @@
 export BUILD_DIR=/home/jlclough/research/high_speed_panel/increase_smb_order/build
 export SRC_DIR=/home/jlclough/research/high_speed_panel/increase_smb_order/src
-export SCOREC_INC=/usr/local/include
+export SCOREC=/usr/local/
+export CXX=/usr/local/bin/mpicxx
 
 cd $BUILD_DIR
 cmake \
   -D SRC_DIR=${SRC_DIR} \
-  -D SCOREC_INC=${SCOREC_INC} \
+  -D SCOREC_INC=${SCOREC}/include \
+  -D SCOREC_LIB=${SCOREC}/lib \
   -D CMAKE_CXX_FLAGS="-g -O3 -Wall -fPIC -lrt" \
+  -D CMAKE_CXX_COMPILER=${CXX} \
   ../
--- increase_smb_order/src/increase_smb_order.cpp
+++ increase_smb_order/src/increase_smb_order.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <apf.h>
+#include <PCU.h>
+#include <pcu_util.h>
 
-int main()
+int main(int argc, char** argv)
 {
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+
   std::cout<< "hello" << std::endl;
+
+
+  PCU_Comm_Free();
+  MPI_Finalize();
   return 0;
 }
