0cc0e27090167a6f5531aaef49414d26ac157ed0
JustinClough <justin.clough1@gmail.com>
2018-09-28 14:55:06 -0700

    Adds for loops for sed and albany execs

--- tet_convergence/run_tests.sh
+++ tet_convergence/run_tests.sh
@@ -5,9 +5,11 @@ TESTING_DIR=$CONVERGENCE_DIR/testing
 LINEAR_DIR=$TESTING_DIR/linear
 QUADRATIC_DIR=$TESTING_DIR/quadratic
 RESULTS_DIR=$TESTING_DIR/results
+ALBANY_EXEC='/home/pogo/Albany/build/src/AlbanyT'
 
-if [ $MESHES_PREPED]
-  # Move to results direcotry
+if [ "$MESHES_PREPED" = true ]
+then
+  # Move to results directory
   cd $RESULTS_DIR
 
   # Copy base input files, also comp_material.yaml
@@ -15,6 +17,40 @@ if [ $MESHES_PREPED]
   cp $CONVERGENCE_DIR/comp_material.yaml $RESULTS_DIR
 
   # For each mesh, modify base_input.yaml to run with that mesh
+  NUM=1
+  for (( i=0; i<$NUM_MESHES; i++))
+  do
+    # First run Linear-Composite Tets
+    sed -e "s/test.smb/..\/linear\/mesh_${NUM}_.smb/"  \
+        -e "s/test.dmg/..\/linear\/model_${NUM}_.dmg/" \
+        -e "s/material.yaml/comp_material.yaml/"       \
+        -e "s/results.vtk/results_comp_${NUM}.vtk/"    \
+        < base_input.yaml                              \
+        > input_comp_${NUM}.yaml
+
+    # Second run Quadratic Tets
+    sed -e "s/test.smb/..\/quadratic\/mesh_${NUM}_.smb/"  \
+        -e "s/test.dmg/..\/quadratic\/model_${NUM}_.dmg/" \
+        -e "s/material.yaml/base_material.yaml/"       \
+        -e "s/results.vtk/results_quad_${NUM}.vtk/"    \
+        < base_input.yaml                              \
+        > input_quad_${NUM}.yaml
+
+    (( NUM*=2 ))
+  done
+
+  # Now run tests
+  NUM=1
+  for (( i=0; i<$NUM_MESHES; i++))
+  do
+    mpirun -n $NUM_PROCS  \
+      ${ALBANY_EXEC} input_comp_${NUM}.yaml
+
+    mpirun -n $NUM_PROCS  \
+      ${ALBANY_EXEC} input_quad_${NUM}.yaml
+
+    (( NUM*=2 ))
+  done
 
 else
   echo "Run make_meshes.sh first!"
