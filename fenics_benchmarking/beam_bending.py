6ad90a95f9a20daa42c55581d19befd964466ffe
JustinClough <justin.clough1@gmail.com>
2018-10-08 16:13:29 -0700

    Adds PETSc options to beam_bending.py

--- fenics_benchmarking/beam_bending.py
+++ fenics_benchmarking/beam_bending.py
@@ -17,10 +17,13 @@ left = Left()
 right = Right()
 #
 lx = 1
-lz = 1
+lz = 0.01
 ly = 0.01
-mesh = BoxMesh(Point(0.0, 0.0, 0.0), Point(lx, ly, lz),20,20,20)
-V = VectorFunctionSpace(mesh, "Lagrange", 1)
+Nx =40
+Nz = 4
+Ny = 4
+mesh = BoxMesh(Point(0.0, 0.0, 0.0), Point(lx, ly, lz),Nx,Ny,Nz)
+V = VectorFunctionSpace(mesh, "Lagrange", 2)
 M = FunctionSpace(mesh, "Lagrange", 1)
 
 # Initialize mesh function for interior domains
@@ -69,17 +72,53 @@ epsilon = 0.5*(grad(u)+grad(u).T) # Deformation gradient
 # Elasticity parameters
 #mu=Expression(("s0 + s1*((x[1] >= 0.0)&(x[1] < lyi)) + s2*((x[1] >= lyi)&(x[1] <= ly))"),s0=1.0,s1=5.0,s2=0.0,ly=3.1,lyi=1.1,element = M.ufl_element())
 
-mu = Constant("1000.0")
-lmbda = Constant("1000.0")
+mu = Constant("1600.0")
+lmbda = Constant("1600.0")
 
 # Cauchy Stress (compressible neo-Hookean model)
 sig = 2*mu*epsilon+lmbda*tr(epsilon)*I
 
 #Weak residual 
-Fn = inner(grad(v),sig)*dx-dot(B, v)*dx - dot(t,v)*ds(2) 
+#Fn = inner(grad(v),sig)*dx-dot(B, v)*dx - dot(t,v)*ds(2) 
 # Solve variational problem
-solve(Fn == 0, u, bcs)
+#solve(Fn == 0, u, bcs)
 
+#petsc test begin
+a = inner(grad(v),sig)*dx
+L = dot(t,v)*ds(2)
+
+# Assemble system
+A = assemble(a)
+b = assemble(L)
+
+# Set PETSc solve type (conjugate gradient) and preconditioner
+# (algebraic multigrid)
+PETScOptions.set("ksp_type", "cg")
+PETScOptions.set("pc_type", "gamg")
+
+# Since we have a singular problem, use SVD solver on the multigrid
+# 'coarse grid'
+PETScOptions.set("mg_coarse_ksp_type", "preonly")
+PETScOptions.set("mg_coarse_pc_type", "svd")
+
+# Set the solver tolerance
+PETScOptions.set("ksp_rtol", 1.0e-8)
+
+# Print PETSc solver configuration
+PETScOptions.set("ksp_view")
+PETScOptions.set("ksp_monitor")
+
+# Create Krylov solver and set operator
+solver = PETScKrylovSolver()
+solver.set_operator(A)
+
+# Set PETSc options on the solver
+solver.set_from_options()
+# Solve
+solver.solve(u.vector(), b)
+
+
+#petsc test end 
 
 # Save solution in VTK format
 file = File("beam/displacement.pvd");
