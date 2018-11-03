9e88cf4103e2c77b7d03c48e2c3312428bd70904
JustinClough <justin.clough1@gmail.com>
2018-11-02 17:40:56 -0700

    Fully debugs beam_bending.py; solves for correct solution with PETSC

--- fenics_benchmarking/beam_bending.py
+++ fenics_benchmarking/beam_bending.py
@@ -30,13 +30,13 @@ lx = 1
 lz = 0.01
 ly = 0.01
 # Number of elements in each direction
-Nx = 40
-Nz =  4
-Ny =  4
+N = 2
+Nx = N*10
+Nz = N
+Ny = N
 # Create the geometry and mesh
-mesh = BoxMesh(Point(0.0, 0.0, 0.0), Point(lx, ly, lz),Nx,Ny,Nz)
+mesh = BoxMesh( Point(0.0, 0.0, 0.0),  Point(lx, ly, lz), Nx ,Ny, Nz)
 V = VectorFunctionSpace(mesh, "Lagrange", 2)
-M = FunctionSpace(mesh, "Lagrange", 1)
 
 # Initialize mesh function for interior domains
 domains = MeshFunction("size_t", mesh, mesh.topology().dim())
@@ -50,9 +50,9 @@ right.mark(boundaries, 2)
 
 # Define Dirichlet boundary (x = 0)
 b0 = Constant((0.0, 0.0, 0.0))
-t = Expression(("scale*0.0",
+t = Expression((" scale*0.0",
                 "-scale*1.0",
-                "scale*0.0"),
+                " scale*0.0"),
                 scale = 0.01, degree=2)
 
 bcl = DirichletBC(V, b0, boundaries, 1)
@@ -63,8 +63,8 @@ dx = Measure('dx', domain=mesh, subdomain_data=domains)
 ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
 
 # Define functions
-v  = TestFunction(V)             # Test function
-u  = TrialFunction(V)                 # Displacement from previous iteration
+v  = TestFunction(V)
+u  = TrialFunction(V)
 
 # Kinematics
 d = len(u)
@@ -80,21 +80,21 @@ lmbda = Constant( E * nu / ( (1.0 + nu)*(1.0 - 2.0 * nu) ))
 # Cauchy Stress (compressible neo-Hookean model)
 sig = 2*mu*epsilon+lmbda*tr(epsilon)*I
 
-a = inner( grad(v),sig )*dx
+a = inner( grad(v), sig )*dx
 L = dot(t,v)*ds(2)
-b = assemble(L)
 
-# Assemble system
-A = PETScMatrix()
-assemble(a, tensor=A)
+# [bc.apply( A, b) for bc in bcs]
+A, b = assemble_system( a, L, bcs)
+tol = 1E-09
 
 # Set PETSc solve type (conjugate gradient) and preconditioner
 # (algebraic multigrid)
 PETScOptions.set("ksp_type", "cg")
-PETScOptions.set("pc_type", "lu")
+PETScOptions.set("pc_type", "ilu")
+PETScOptions.set("pc_factor_levels", 0)
 
 # Set the solver tolerance
-PETScOptions.set("ksp_rtol", 1.0e-7)
+PETScOptions.set("ksp_rtol", tol)
 
 # Print PETSc solver configuration
 PETScOptions.set("ksp_view")
@@ -102,15 +102,14 @@ PETScOptions.set("ksp_monitor")
 
 # Create Krylov solver and set operator
 solver = PETScKrylovSolver()
-solver.set_operator(A)
 
 # Set PETSc options on the solver
 solver.set_from_options()
 # Solve
-u = Function( V)
-solver.solve( u.vector(), b)
+u_h = Function( V)
+solver.solve( A, u_h.vector(), b)
 
 # Save solution in VTK format
 file = File("results_beam/displacement.pvd");
-u.rename('Displacement', 'label')
-file << u;
+u_h.rename('Displacement', 'label')
+file << u_h;
