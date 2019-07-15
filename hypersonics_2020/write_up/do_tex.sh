112477f5a26d47bc9fcc11430614246f213d284a
JustinClough <justin.clough1@gmail.com>
2019-07-15 11:09:37 -0400

    Adds a do_tex.sh for hype_2020

---
+++ hypersonics_2020/write_up/do_tex.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DIR=${PWD}
+
+ROOT_DIR=${HOME}/research/high_speed_panel/hypersonics_2020/write_up
+BIB_DIR=bib
+DATA_DIR=data
+IMAGES_DIR=images
+
+MY_PROJ_DIR=${ROOT_DIR}
+MY_BIB_DIR=${ROOT_DIR}/${BIB_DIR}
+MY_DATA_DIR=${ROOT_DIR}/${DATA_DIR}
+MY_IMAGE_DIR=${ROOT_DIR}/${IMAGES_DIR}
+
+PROJ_TITLE=grasp_report
+
+TEX_EXTENSION=.tex
+AUX_EXTENSION=.aux
+PDF_EXTENSION=.pdf
+
+WORKSPACE=${DIR}/workspace
+
+TARGET=${PROJ_TITLE}
+TARGET_DIR=${MY_PROJ_DIR}
+
+cd $WORKSPACE
+rm -r $WORKSPACE/*
+
+cat $MY_BIB_DIR/*          >> ${WORKSPACE}/monolith.bib
+cp -r ${MY_DATA_DIR}          ${WORKSPACE}/${DATA_DIR}
+cp -r ${MY_IMAGE_DIR}         ${WORKSPACE}/${IMAGES_DIR}
+
+cd ${WORKSPACE}/${DATA_DIR}
+cd ${WORKSPACE}
+
+cp ${TARGET_DIR}/${TARGET}${TEX_EXTENSION} ${WORKSPACE}/${TARGET}${TEX_EXTENSION}
+
+LOCAL_TEX=${TARGET}${TEX_EXTENSION}
+LOCAL_AUX=${TARGET}${AUX_EXTENSION}
+LOCAL_PDF=${TARGET}${PDF_EXTENSION}
+
+pdflatex $LOCAL_TEX
+bibtex   $LOCAL_AUX
+pdflatex $LOCAL_TEX
+pdflatex $LOCAL_TEX
+
+gio open ${LOCAL_PDF}
+
+cd ${DIR}
