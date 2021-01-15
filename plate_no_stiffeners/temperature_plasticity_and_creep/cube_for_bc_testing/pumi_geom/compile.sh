f65e5025e3d1b7a9741d1cf604505c327893b366
JustinClough <justin.clough1@gmail.com>
2021-01-14 16:43:21 -0800

    Removes cube for bc testing dir since no longer needed

--- plate_no_stiffeners/temperature_plasticity_and_creep/cube_for_bc_testing/pumi_geom/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm quarter_plate.dmg quarter_plate.geo
-
-mpicxx make_geo_dmg_pair.cpp -o make_geo_dmg_pair.exe -I/home/pogo/gmodel/install/include -L/home/pogo/gmodel/install/lib -lgmodel
