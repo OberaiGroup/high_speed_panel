e9a8b0d140a03f2eb25baeaf58c7ae36722628d2
JustinClough <justin.clough1@gmail.com>
2021-02-26 17:27:15 -0800

    Adds input files for quarterplate with interpolated temperature

---
+++ plate_no_stiffeners/plasticity_and_creep/hpc_analytical_temperature/run_panel_analytical_temperaure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -J flat_25s_2d_16c_interp
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=48:00:00
+#SBATCH --mail-user=jlclough@usc.edu
+#SBATCH --mail-type=all
+#SBATCH --constraint="xeon-6130"
+#SBATCH --account=aoberai_286
+
+module purge
+module load gcc/9.2.0
+module load openmpi/4.0.2 pmix/3.1.3 cmake/3.15.4 boost/1.70.0
+
+ulimit -s unlimited
+
+export OMP_NUM_THREADS=16
+
+export dir=/project/aoberai_286/jlclough/high_speed_panel/plate_no_stiffeners/plasticity_and_creep/hpc_analytical_temperature
+cd $dir
+
+mpirun "-n" 16 $ALBANY_EXE ${dir}/input.yaml 
+
